5e48ece54ce85fe7f771bb2cd8f92b5917b137af
AlmasBanuA <94681887+AlmasBanuA@users.noreply.github.com>
2022-02-20 08:43:43 +0530

    printing the Euclidean distance from the point (x, y)

--- src/bridgelabz/Distance.java
+++ src/bridgelabz/Distance.java
@@ -1,17 +1,27 @@
 package bridgelabz;
 
+/* 
+* importing the scanner class
+*/
 import java.util.Scanner;
 
 public class Distance {
 
 	public static void main(String[] args) {
 
+		/* 
+		*takes two integer command-line arguments x and y
+		*/
+		
 		int x1;
 		int x2;
 		int y1;
 		int y2;
 		double Dist;
 
+		/*
+		* creating scanner object and prints inpuut from user
+		*/
 		Scanner sc = new Scanner(System.in);
 
 		System.out.println("enter x1 point");
@@ -30,6 +40,9 @@ public class Distance {
 
 		y2 = sc.nextInt();
 
+		/*
+		*  prints the Euclidean distance from the point (x, y) to the origin (0, 0) from distance formula
+		*/
 		Dist = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
 
 		System.out.println("The distance between" + "(" + x1 + "," + y1 + ")," + "(" + x2 + "," + y2 + ") is:" + Dist);
