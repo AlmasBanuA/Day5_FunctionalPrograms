1d89333805d60fe33d7e19717867339c154862e5
AlmasBanuA <94681887+AlmasBanuA@users.noreply.github.com>
2022-02-20 09:13:22 +0530

    Print function to print 2 Dimensional Array.

--- src/bridgelabz/TwoDimensionalArray.java
+++ src/bridgelabz/TwoDimensionalArray.java
@@ -1,11 +1,17 @@
 package bridgelabz;
 
+/*
+* importing scanner class
+*/
 import java.util.Scanner;
 
 public class TwoDimensionalArray {
 
 	public static void main(String[] args) {
-
+		
+		/*
+		*  create scanner object
+		*/
 		Scanner sc = new Scanner(System.in);
 
 		System.out.println("Enter the rows");
@@ -13,12 +19,20 @@ public class TwoDimensionalArray {
 		System.out.println("Enter the columns");
 		int m = sc.nextInt();
 		System.out.println("You are creating array of size: " + m + "x" + n);
+		
+		/*
+		*  A library for reading in 2D arrays of integers, doubles, or booleans from
+		*  standard input and printing them out to standard output.
+		*/
 		System.out.println("Cases for 2D Array ....");
 		System.out.println("1  ==>> Integer");
 		System.out.println("2  ==>> Double");
 		System.out.println("3  ==>> Boolean");
 		int Case = sc.nextInt();
-
+		
+		/*
+		* using switch case for integer type array in rows and columns
+		*/
 		switch (Case) {
 		case 1:
 			System.out.println("For interger type array, please enter numbers for rows and columns");
@@ -34,6 +48,11 @@ public class TwoDimensionalArray {
 				}
 				System.out.println(" ");
 			}
+				
+		
+		* /*
+		* using switch case for double type array in rows and columns
+		*/
 		case 2:
 			System.out.println("For double type array, please enter numbers for rows and columns");
 
@@ -49,7 +68,10 @@ public class TwoDimensionalArray {
 				}
 				System.out.println(" ");
 			}
-
+				
+		/*
+		* using switch case for boolen type array in rows and columns
+		*/
 		case 3:
 			System.out.println("For boolean type array, please enter values for rows and columns");
 
@@ -65,6 +87,10 @@ public class TwoDimensionalArray {
 				}
 				System.out.println(" ");
 			}
+				
+		/*
+		*  if three cases will not executed then enters for default statement
+		*/
 		default:
 			System.out.println("INVALID INPUT ");
 		}
